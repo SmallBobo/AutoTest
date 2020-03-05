69b40d0705a2db42bbeacce799f165c27adde7f9
SmallBobo <871690963@qq.com>
2020-03-05 19:52:22 +0800

    1122

---
+++ Chapter/src/main/java/com/course/testng/ParameterTest.java
@@ -0,0 +1,17 @@
+package com.course.testng;
+
+
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+
+public class ParameterTest {
+
+    @Test
+    @Parameters({"name","age"})
+    public void paramTest(String name,int age){
+
+        System.out.println("name = "+name+"; age="+age);
+        System.out.printf("paramTest方法的ThreadID：%s%n",Thread.currentThread().getId());
+    }
+}
---
+++ Chapter6/src/main/java/com/extentreport/test/TestMethodsDemo.java
@@ -0,0 +1,25 @@
+package com.extentreport.test;
+
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TestMethodsDemo {
+
+    //断言
+    @Test
+    public void test1(){
+        Assert.assertEquals(1,1);
+    }
+
+    public void test3(){
+        Assert.assertEquals(1,2);
+    }
+
+    //日志
+    @Test
+    public void test2(){
+        Reporter.log("这是人为添加的日志信息1111111111");
+        throw new RuntimeException();
+    }
+}
